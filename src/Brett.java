a4d13a0127d5bcbf82147521735999ae671d207f
sgvfhgiu5367958 <xmaxx3000@gmail.com>
2022-05-25 09:05:56 +0200

    hasgvrbera

--- src/Brett.java
+++ src/Brett.java
@@ -5,10 +5,17 @@ public class Brett {
         feld = new Figur[8][8];
         feldFuellen();
     }
-    //LULulu
+
     private void feldFuellen() {
         
     }
 
-    //eresgirea
+    public void move(Figur figur, int x, int y,) {      
+       if (feld[x][y] == null) {
+           feld[x][y] == feld[figur.getPositionX()][figur.getPositionY()]
+       }
+        
+        
+    }
+
 }
