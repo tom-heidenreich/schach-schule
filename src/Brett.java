99d39b5df4d770f29428549f32ad8c186fffb13a
sgvfhgiu5367958 <xmaxx3000@gmail.com>
2022-05-25 09:25:19 +0200

    sgtrdvgt

--- src/Brett.java
+++ src/Brett.java
@@ -3,11 +3,16 @@ public class Brett {
 
     public Brett() {
         feld = new Figur[8][8];
+        String aufstellung[] = {"br", "bn","bb", "bq", "bk", "bb", "bn" "br"}
         feldFuellen();
     }
 
     private void feldFuellen() {
-        
+        for (int k = 0; k < 8; k++) {
+            for (int s = 6; k < 8; k++) {
+            feld = new Figur [s][k] = aufstellung[k];
+            }
+        }
     }
 
     public void move(Figur figur, int x, int y,) {      
