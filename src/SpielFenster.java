7a812a56b79cbd94f1f7b7ea15b389b128c9d0ab
Tom Heidenreich <heidenreichdd@gmail.com>
2022-06-01 08:15:14 +0200

    man kann klicken

--- src/SpielFenster.java
+++ src/SpielFenster.java
@@ -16,6 +16,11 @@ public class SpielFenster {
 
     private final BufferStrategy bufferStrategy;
 
+    private final int paddingX = 200;
+    private final int paddingY = 100;
+
+    private final int size = 100;
+
     public SpielFenster(Schach schach) {
 
         this.schach = schach;
@@ -27,8 +32,23 @@ public class SpielFenster {
         frame.setResizable(false);
         frame.setSize(dimension);
 
+        // click listener
+        frame.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                int x = evt.getX();
+                int y = evt.getY();
+                // if inside board
+                if(x - paddingX > 0 && x < dimension.width - paddingX && y > 0 && y < dimension.height - paddingY) {
+                    int xPos = (x - 1) / size;
+                    int yPos = (y - 1) / size;
+                }
+            }
+        });
+
         frame.setVisible(true);
 
+        // buffered frames
         frame.createBufferStrategy(2);
         bufferStrategy = frame.getBufferStrategy();
 
@@ -76,7 +96,7 @@ public class SpielFenster {
                 } else {
                     graphics.setColor(Color.black);
                 }
-                graphics.fillRect(i * 100 + 200, j * 100 + 100, 100, 100);
+                graphics.fillRect(i * size + paddingX, j * size + paddingY, size, size);
             }
         }
 
