8fe917846da30d970ffeeb877dd507b095b24886
sgvfhgiu5367958 <xmaxx3000@gmail.com>
2022-06-01 08:12:47 +0200

    brett

--- src/Brett.java
+++ src/Brett.java
@@ -3,21 +3,23 @@ public class Brett {
 
     public Brett() {
         feld = new Figur[8][8];
-        String aufstellung[] = {"br", "bn","bb", "bq", "bk", "bb", "bn" "br"}
+        String aufstellung[] = {"r", "n","b", "q", "k", "b", "n" "r"}
         feldFuellen();
     }
 
     private void feldFuellen() {
         for (int k = 0; k < 8; k++) {
-            for (int s = 6; k < 8; k++) {
-            feld = new Figur [s][k] = aufstellung[k];
-            }
+             feld[7][k] = new Figur(k, 7, aufstellung[k], "b");
+             feld[0][k] = new Figur(k, 0, aufstellung[k], "w");
+             feld[1][k] = new Figur(k, 1, "p", "w");
+             feld[6][k] = new Figur(k, 6, "p", "b");
+
         }
     }
 
     public void move(Figur figur, int x, int y,) {      
        if (feld[x][y] == null) {
-           feld[x][y] == feld[figur.getPositionX()][figur.getPositionY()]
+           feld[x][y] == feld[figur.getPositionX()][figur.getPositionY()];
        }
         
         
