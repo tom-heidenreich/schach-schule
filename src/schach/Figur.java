9b2f4c73fefe1eabc2dd1b4f487f478760fd6047
Indianer-Jonas <jonas.wurzschmitt@outlook.de>
2022-06-03 11:03:39 +0200

    getmoves

--- src/schach/Figur.java
+++ src/schach/Figur.java
@@ -54,5 +54,57 @@ public class Figur {
     public FigurFarbe getFarbe() {
         return farbe;
     }
+    //give all possible moves in a two dimensional array
+    public boolean[][] getMoves(Figur[][] feld) {
+        boolean[][] moves = new boolean[8][8];
+        //if type is läufer
+        if (type == FigurType.LÄUFER)return boolean[][] läufer(feld)); 
+        //if type is Springer
+        else if (type == FigurType.SPRINGER) return boolean[][] springer(feld);
+        //if type is Turm
+        else if (type == FigurType.TURM) return boolean[][] turm(feld);
+        //if type is Dame
+        else if (type == FigurType.DAME) return boolean[][] dame(feld);
+        //if type is König
+        else if (type == FigurType.KÖNIG) return boolean[][] könig(feld);
+        //if type is Bauern
+        else if (type == FigurType.BAUER) return boolean[][] bauer(feld);
+        else return null;
+    }
 
+    //if type is läufer
+    private boolean[][] läufer(Figur[][] feld) {
+        boolean[][] moves = new boolean[8][8];
+        //for every x = y if no figure is in the way and the position is in the board
+        for (int i = -7; i < 7; i++) {
+            if (x + i < 8 && x + i > 0 && y + i < 8 && y + i > 0 && feld[x + i][y + i] == null) {
+                moves[x + i][y + i] = true;
+            }
+        }
+        return moves;
+    }
+
+    //if type is turm
+    private boolean[][] turm(Figur[][] feld) {
+        boolean[][] moves = new boolean[8][8];
+        //go from x and y to the right
+        for(int i = x + 1; i + x < 8+; i++) {
+            if (feld[i][y] == null) {
+                moves[i][y] = true;
+            } else if (feld[i][y].getFarbe() != farbe) {
+                moves[i][y] = true;
+                break;
+            } else {
+                break;
+            }
+        }
+        for (int i = -7; i < 7; i++) {
+            if (x + i < 8 && x + i > 0 && feld[x + i][y] == null) {
+                moves[x + i][y] = true;
+            }
+            if (y + i < 8 && y + i > 0 && feld[x + i][y] == null) {
+                moves[x][y + i] = true;
+            }
+        }
+    }
 }
